953a8f80505a0fa5fb554f96bae7ab78cb4bfd59
shell <shellipov@mail.ru>
2021-07-02 16:21:56 +0300

    add test route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import authRouter from "./authRouter.js";
 import eventRouter from "./eventRouter.js";
 import photoRouter from "./photoRouter.js";
+import testRouter from './testRouter.js'
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/event", eventRouter);
 router.use("/photo", photoRouter);
+router.use("/test", testRouter);
 
 export default router;
