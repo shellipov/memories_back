4185b21546262834aa3f6d0d341468b0ab54fb18
shell <shellipov@mail.ru>
2021-07-05 21:23:29 +0300

    origin cors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(
   }
 );
 
-app.use(cors({ origin: "https://shellipov.github.io/memories_front", credentials: true }));
+app.use(cors({ origin: "https://shellipov.github.io/", credentials: true }));
 
 app.use(express.json());
 
